7eaaa3d1978eb32ab6666b06affcbf802f709163
Theodoros Siklafidis <theodoros.siklafidis@sbtechnologies.org>
2020-10-03 21:28:41 +0300

    Add basic enumeration for available epidemic models

---
+++ src/utils/utlEnum_u.pas
@@ -0,0 +1,39 @@
+{
+                Copyright (C) 2020 Theodoros Siklafidis
+
+    This file is part of BVS.
+
+    BVS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BVS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BVS. If not, see <https://www.gnu.org/licenses/>.
+}
+
+unit utlEnum_u;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+const
+  { Basic enumeration for available epidemic models }
+  NumberOfAvailableModels = 1;
+  SIR = 'SIR';
+  SIS = 'SIS';
+  AvailableModels: Array[0..NumberOfAvailableModels] of String = (SIR, SIS);
+
+implementation
+
+end.
+
